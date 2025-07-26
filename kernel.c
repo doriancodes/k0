bdec5b57c3e901c2cacf52b0cae12bb0c87e6b19
doriancodes <doriancodes@easy.box>
2025-07-27 00:25:55 +0200

    kprint and clear_screen functions for debugging

--- kernel.c
+++ kernel.c
@@ -1,11 +1,33 @@
-void kernel_main(void) {
-  const char *str = "Hello!";
-  char *vidmem = (char *)0xb8000;
-  for (int i = 0; str[i] != '\0'; i++) {
-    vidmem[i * 2] = str[i];
-    vidmem[i * 2 + 1] = 0x07; // Light grey on black
-  }
+void kprint(const char *str) {
+  volatile char *vidmem = (char *)0xb8000;
+  static int row = 0, col = 0;
 
+  while (*str) {
+    if (*str == '\n') {
+      row++;
+      col = 0;
+      str++;
+    } else {
+      int pos = (row * 80 + col) * 2;
+      vidmem[pos] = *str++;
+      vidmem[pos + 1] = *str % 16; // 0x07 if gray
+      col++;
+    }
+  }
+}
+
+void clear_screen() {
+  volatile char *vidmem = (char *)0xb8000;
+  for (int i = 0; i < 80 * 25; i++) {
+    vidmem[i * 2] = ' ';
+    vidmem[i * 2 + 1] = 0x07;
+  }
+}
+
+void kernel_main(void) {
+  clear_screen();
+  kprint("Hello, world!\n");
+  kprint("Welcome to Kerny OS!\n");
   while (1) {
   }
 }
